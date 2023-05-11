5a49199e3739c922b719ae8f752cd51ab1f6dcc8
Layak Ali <93964740+Layak-Ali@users.noreply.github.com>
2023-05-11 19:28:20 +0530

    Added files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let total = count + " - "
+    saveEl.textContent += total
+    countEl.textContent = 0
+    count = 0
+}
